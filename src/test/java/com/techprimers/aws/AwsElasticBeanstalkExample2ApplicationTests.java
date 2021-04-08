eaf642f0d4ee15bf29db37c27a4a6d7b80c2c5b0
aa18crt <78596498+aa18crt@users.noreply.github.com>
2021-04-08 10:04:18 +0100

    Update AwsElasticBeanstalkExample2ApplicationTests.java

--- src/test/java/com/techprimers/aws/AwsElasticBeanstalkExample2ApplicationTests.java
+++ src/test/java/com/techprimers/aws/AwsElasticBeanstalkExample2ApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class AwsElasticBeanstalkExample2ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads Alex() {
 	}
 
 }
